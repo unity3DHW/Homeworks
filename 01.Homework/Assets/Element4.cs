918e0000a33c0db6171a3ee09268a615886686ee
iuli1478 <iuli.iuli17@gmail.com>
2014-10-28 06:33:04 +0200

    Homework 1

---
+++ 01.Homework/Assets/Element4.cs
@@ -0,0 +1,41 @@
+﻿namespace Assets.MoveElement1
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public class Element4 : Movable
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float speed = 0.05f;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                MuveUp(speed);
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                MuveDown(speed);
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                MuveLeft(speed);
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                MuveRight(speed);
+            }
+        }
+    }
+}
---
+++ 01.Homework/Assets/Move/Movable.cs
@@ -0,0 +1,36 @@
+﻿namespace Assets
+{
+    using UnityEngine;
+    using System.Collections;
+
+    public abstract class Movable : MonoBehaviour
+    {
+        public Vector3 MuveUp(float speed)
+        {
+            Vector3 position = this.transform.position;
+            position.z += speed;
+            return this.transform.position = position;
+        }
+
+        public Vector3 MuveDown(float speed)
+        {
+            Vector3 position = this.transform.position;
+            position.z -= speed;
+            return this.transform.position = position;
+        }
+
+        public Vector3 MuveLeft(float speed)
+        {
+            Vector3 position = this.transform.position;
+            position.x -= speed;
+            return this.transform.position = position;
+        }
+
+        public Vector3 MuveRight(float speed)
+        {
+            Vector3 position = this.transform.position;
+            position.x += speed;
+            return this.transform.position = position;
+        }
+    }
+}
